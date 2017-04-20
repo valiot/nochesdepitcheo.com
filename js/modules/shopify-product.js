ecfebe7b63cae797535ca45f505007ea6987fbe5
Jorge Escamilla <jorge.escamilla89@gmail.com>
2017-04-20 17:18:45 -0500

    Refactor[shopify-product.js]

--- js/modules/shopify-product.js
+++ js/modules/shopify-product.js
@@ -9,48 +9,24 @@ $(function() {
   var normalTshirt = "9012626642";
   var longTshirt = "9012631058";
 
-  shopClient.fetchProduct(comfyTshirt).then(function(product) {
-    var comfyTshirtContent =
-    "<figure class='product-image'>" +
-      "<img src='" + product.selectedVariantImage.src + "' >" +
-      "<a class='btn btn--action' href='" + product.selectedVariant.checkoutUrl(1) +
-      "'>COMPRAR</a>" +
-    "</figure>" +
-    "<div class='product-info'>" +
-      "<p class='product-name'>" + product.title + "</p>" +
-      "<p class='product-price'>$2OOmx</p>" +
-    "</div>";
+  function fetchShopifyProduct(id, div) {
+    shopClient.fetchProduct(id).then(function(product) {
+      var content =
+      "<figure class='product-image'>" +
+        "<img src='" + product.selectedVariantImage.src + "' >" +
+        "<a class='btn btn--action' href='" + product.selectedVariant.checkoutUrl(1) +
+        "'>COMPRAR</a>" +
+      "</figure>" +
+      "<div class='product-info'>" +
+        "<p class='product-name'>" + product.title + "</p>" +
+        "<p class='product-price'>$2OOmx</p>" +
+      "</div>";
 
-    $('#comfyTshirt').html(comfyTshirtContent); 
-  });
+      $(div).html(content); 
+    });
+  }
 
-  shopClient.fetchProduct(normalTshirt).then(function(product) {
-    var normalTshirtContent =
-    "<figure class='product-image'>" +
-      "<img src='" + product.selectedVariantImage.src + "' >" +
-      "<a class='btn btn--action' href='" + product.selectedVariant.checkoutUrl(1) +
-      "'>COMPRAR</a>" +
-    "</figure>" +
-    "<div class='product-info'>" +
-      "<p class='product-name'>" + product.title + "</p>" +
-      "<p class='product-price'>$2OOmx</p>" +
-    "</div>";
-
-    $('#normalTshirt').html(normalTshirtContent); 
-  });
-
-  shopClient.fetchProduct(longTshirt).then(function(product) {
-    var longTshirtContent =
-    "<figure class='product-image'>" +
-      "<img src='" + product.selectedVariantImage.src + "' >" +
-      "<a class='btn btn--action' href='" + product.selectedVariant.checkoutUrl(1) +
-      "'>COMPRAR</a>" +
-    "</figure>" +
-    "<div class='product-info'>" +
-      "<p class='product-name'>" + product.title + "</p>" +
-      "<p class='product-price'>$2OOmx</p>" +
-    "</div>";
-
-    $('#longTshirt').html(longTshirtContent); 
-  });
+  fetchShopifyProduct(comfyTshirt, "#comfyTshirt");
+  fetchShopifyProduct(normalTshirt, "#normalTshirt");
+  fetchShopifyProduct(longTshirt, "#longTshirt");
 });

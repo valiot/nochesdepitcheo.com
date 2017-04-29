af42a747e92ba9608a9eae375abcb23f53a89a10
esnilara <esteban@esnilara.io>
2017-04-29 13:51:23 -0500

    Feature[shopify-cart]: Add cart markup with css & javascript adjustments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 $(function () {
-
   var tshirtCollectionId = '379094994';
   var accessToken = '70713926e14ee6c0b19f901fe0e30efa';
   var domain = 'noches-de-pitcheo.myshopify.com';
@@ -10,19 +9,59 @@ $(function () {
   // Build new ShopifyBuy client.
   var shopClient = ShopifyBuy.buildClient({ accessToken: accessToken, domain: domain, appId: appId });
 
-  // Fetch products based on t-shirt collection.
+  // Fetch products based on tshirt collection and init.
   shopClient.fetchQueryProducts({ collection_id: tshirtCollectionId }).then(function (products) {
     return products.forEach(function (product) {
       createDOMProductItems(product);
+      generateDOMProductSelector(product);
+      attachOnVariantSelectListeners(product);
     });
   }).catch(function () {
     console.log('Request Failed');
   });
 
-  // Create DOM Product List based on product Template.
+  // Create DOM product list based on product template.
   function createDOMProductItems(product) {
-    var productDOMTemplate = '\n      <div class="product" id="product-' + product.id + '">\n        <figure class="product-image">\n          <img src="' + product.selectedVariantImage.src + '" alt="' + product.title + '">\n          <a href="' + product.selectedVariant.checkoutUrl(1) + '" class="btn btn--action">\n            COMPRAR\n          </a>\n        </figure>\n        <div class="product-info">\n          <p class="product-name">' + product.title + '</p>\n          <p class="product-price">' + product.selectedVariant.formattedPrice + ' MXN</p>\n        </div>\n      </div>\n    ';
+    var productDOMTemplate = '\n      <div class="product" id="product-' + product.id + '">\n        <figure class="variant-image">\n          <img src="' + product.selectedVariantImage.src + '" alt="' + product.title + '">\n          <a href="' + product.selectedVariant.checkoutUrl(1) + '" class="btn btn--action">\n            COMPRAR\n          </a>\n        </figure>\n\n        <div class="product-info">\n          <p class="product-title">' + product.title + '</p>\n          <p class="variant-price">' + product.selectedVariant.formattedPrice + '</p>\n          <div class="variant-selector"></div>\n        </div>\n      </div>\n    ';
 
     $('#product-list').append(productDOMTemplate);
+  }
+
+  // Generate product variant element selectors.
+  function generateSelectors(product) {
+    var elements = product.options.map(function (option) {
+      var optionsHtml = option.values.map(function (value) {
+        return '<option value="' + value + '">' + value + '</option>';
+      });
+
+      return '\n        <select class="select" name="' + option.name + '">' + optionsHtml + '</select>\n      ';
+    });
+
+    return elements;
+  }
+
+  // Insert product variant selector into DOM.
+  function generateDOMProductSelector(product) {
+    $('#product-' + product.id + ' .variant-selector').html(generateSelectors(product));
+  }
+
+  // Variant option change event handler.
+  function attachOnVariantSelectListeners(product) {
+    var productElement = '#product-' + product.id;
+
+    $(productElement + ' .variant-selector').on('change', 'select', function (event) {
+      var $element = $(event.target);
+      var name = $element.attr('name');
+      var value = $element.val();
+      var selectedVariant = product.selectedVariant;
+      var selectedVariantImage = product.selectedVariantImage;
+      var checkoutUrl = selectedVariant.checkoutUrl(1);
+
+      product.options.filter(function (option) {
+        return option.name === name;
+      })[0].selected = value;
+
+      // TODO: Update cart model.
+    });
   }
 });
--- js/modules/shopify-product.js
+++ js/modules/shopify-product.js
@@ -1,5 +1,4 @@
 $(function() {
-
   const tshirtCollectionId = '379094994';
   const accessToken        = '70713926e14ee6c0b19f901fe0e30efa';
   const domain             = 'noches-de-pitcheo.myshopify.com';
@@ -8,32 +7,76 @@ $(function() {
   // Build new ShopifyBuy client.
   const shopClient = ShopifyBuy.buildClient({ accessToken, domain, appId });
 
-  // Fetch products based on t-shirt collection.
+  // Fetch products based on tshirt collection and init.
   shopClient.fetchQueryProducts({ collection_id: tshirtCollectionId }).then((products) => {
     return products.forEach((product) => {
       createDOMProductItems(product);
+      generateDOMProductSelector(product);
+      attachOnVariantSelectListeners(product);
     });
   }).catch(() => {
     console.log('Request Failed');
   });
 
-  // Create DOM Product List based on product Template.
+  // Create DOM product list based on product template.
   function createDOMProductItems(product) {
     let productDOMTemplate = `
       <div class="product" id="product-${product.id}">
-        <figure class="product-image">
+        <figure class="variant-image">
           <img src="${product.selectedVariantImage.src}" alt="${product.title}">
           <a href="${product.selectedVariant.checkoutUrl(1)}" class="btn btn--action">
             COMPRAR
           </a>
         </figure>
+
         <div class="product-info">
-          <p class="product-name">${product.title}</p>
-          <p class="product-price">${product.selectedVariant.formattedPrice} MXN</p>
+          <p class="product-title">${product.title}</p>
+          <p class="variant-price">${product.selectedVariant.formattedPrice}</p>
+          <div class="variant-selector"></div>
         </div>
       </div>
     `;
 
     $('#product-list').append(productDOMTemplate);
+  }
+
+  // Generate product variant element selectors.
+  function generateSelectors(product) {
+    let elements = product.options.map((option) => {
+      let optionsHtml = option.values.map((value) => {
+        return `<option value="${value}">${value}</option>`;
+      });
+
+      return `
+        <select class="select" name="${option.name}">${optionsHtml}</select>
+      `;
+    });
+
+    return elements;
+  }
+
+  // Insert product variant selector into DOM.
+  function generateDOMProductSelector(product) {
+    $(`#product-${product.id} .variant-selector`).html(generateSelectors(product));
+  }
+
+  // Variant option change event handler.
+  function attachOnVariantSelectListeners(product) {
+    let productElement = `#product-${product.id}`;
+
+    $(`${productElement} .variant-selector`).on('change', 'select', (event) => {
+      let $element             = $(event.target);
+      let name                 = $element.attr('name');
+      let value                = $element.val();
+      let selectedVariant      = product.selectedVariant;
+      let selectedVariantImage = product.selectedVariantImage;
+      let checkoutUrl          = selectedVariant.checkoutUrl(1);
+
+      product.options.filter((option) => {
+        return option.name === name;
+      })[0].selected = value;
+
+      // TODO: Update cart model.
+    });
   }
 });

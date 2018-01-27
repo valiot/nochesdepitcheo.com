9805350434e91f195e1067cd6002e6d87d946e78
Jorge Escamilla <jorge.escamilla89@gmail.com>
2018-01-27 16:44:51 -0600

    Change tag-maganer loading

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';j.async = true;j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-KMPZ6SH');
+'use strict';
+
 $(function () {
   var tshirtCollectionId = '379094994';
   var accessToken = '70713926e14ee6c0b19f901fe0e30efa';

d5025daa29ca01824382778323d07fec61fdf851
Francis Vallejo <francisvl94@gmail.com>
2018-02-22 21:32:17 -0600

    fixes

--- js/module/slider.js
+++ js/module/slider.js
@@ -1,8 +1,5 @@
-$(document).ready(function () {
-  console.log('Si funciona jQuery :)');
-
-  $('.mySlider').unslider( {
-    nav: false,
-    speed: 1000,
-    infinite: true
+function productImages() {
+  $('.product').on('click', '.btn-buy', function(){
+    console.log('Si funciona jQuery :)');
   });
+};
